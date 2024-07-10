11535b50a00a780d8ed31bcd14f179fb4cbb99c2
dev-jay-jay <johngratia2@gmail.com>
2024-07-10 23:11:04 +0100

    New onboarding design

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
       bgSidebar: {
         'custom-gradient': 'linear-gradient(57%, #11468F 3%, #11468F 100%)',
       },
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(circle, #3b82f6, #3e81f1, #4180eb, #447fe6, #477ee1, #487ddc, #4a7cd8, #4b7bd3, #4c7ace, #4d79c8, #4e77c3, #4f76be)',
+      },
+      letterSpacing: {
+        'extra-wide': '0.09rem',
+      }
     },
   },
   variants: {
